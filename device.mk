1fcdefcd4a4ab64b70ccbec0488f3cdc786b2ddf
Woomymy <github@woomy.ovh>
2022-06-21 17:52:55 +0200

    evergo: bootctrl: Update to version 1.2 and build it
    * Implement getActiveBootSlot
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -42,6 +42,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
 
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
