e41c90bff978690bd2322704f890b347cb14fd8c
Sushrut1101 <guptasushrut@gmail.com>
2022-12-05 19:23:28 +0530

    evergo: Set `TW_NO_FASTBOOT_BOOT` to true
    * Our device does not support hotbooting an image using fastboot boot,
      so `twrpfastboot=1` cmdline cause various issues like battery level
      detection, unable to reboot to system etc. When this flag is set to
      true, twrpfastboot=1 isn't added into the kernel cmdline and therefore
      fixes those issues.
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,9 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
 
+# This device does not support fastboot boot, do *NOT* remove!
+TW_NO_FASTBOOT_BOOT := true
+
 # Debug
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
